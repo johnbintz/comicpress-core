5d66b8b589c1e069bfdf39b36dcd5794ae491406
John Bintz <john@coswellproductions.com>
2009-11-02 22:21:53 -0500

    switch to prototype

--- js/Storyline.js
+++ js/Storyline.js
@@ -2,24 +2,33 @@ var Storyline = {};
 
 Storyline.get_order = function() {
 	var order = []
-	jQuery('#storyline-sorter .cp-category-info').each(function() {
-		var matches = this.className.match(/category-([0-9\/]+)/);
+	$$('#storyline-sorter .cp-category-info').each(function(info) {
+		var matches = info.id.match(/category_([0-9\/]+)/);
 		if (matches) { order.push(matches[1]); }
 	});
-	jQuery('input[name=cp[storyline_order]]').attr('value', order.join(','));
+  $$('input[name*=storyline_order]').pop().value = order.join(',');
 };
 
 Storyline.setup = function() {
-	jQuery('.cp-children').sortable({
-		handle: 'span',
-		cursor: 'move',
-		placeholder: 'placeholder',
-		forcePlaceholderSize: true,
-		opacity: 0.4,
-		stop: function(e, ui) {
-			Storyline.get_order();
-		}
-	});
-};
+  var i = 0;
+  var depths = {};
+  $$('#storyline-sorter .cp-children').each(function(ch) {
+    ch.id = 'children-' + i;
+    var depth = ch.ancestors().length;
+    if (!depths[depth]) { depths[depth] = []; }
+    depths[depth].push(ch);
+    ++i;
+  });
 
-jQuery(function() { Storyline.get_order(); });
+  depths = $H(depths);
+
+  depths.keys().sort(function(a,b) { return b - a; }).each(function(depth) {
+    depths.get(depth).each(function(ch) {
+      Sortable.create(ch.id, {
+        tag: 'div',
+        handle: 'span',
+        onUpdate: Storyline.get_order
+      });
+    });
+  });
+};
