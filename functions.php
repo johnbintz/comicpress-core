88b9e6cf8315fd04fe2b98037579f6f3f76e9b58
John Bintz <john@dawn.local>
2009-11-07 12:23:10 -0500

    remove layout stuff

--- functions.php
+++ functions.php
@@ -44,22 +44,6 @@ function __comicpress_init() {
   }
 }
 
-function comicpress_init() {
-  global $post, $comicpress;
-  
-  if (!empty($post)) {
-    if (in_comic_category() && $comicpress->is_multicomic() && !is_index()) {
-      $comicpress->setup_multicomic_partial_paths($post->ID);
-    }
-  }
-  
-  $comicpress->partial_paths[] = get_template_directory() . '/partials';
-}
-
-function comicpress_get_header() {
-  get_header();
-}
-
 function include_partial($partials = '') {
   global $comicpress, $post, $nav_comics;
   
@@ -80,9 +64,6 @@ function include_partial($partials = '') {
   }
 }
 
-function in_comic_category() {
-  global $post, $comicpress;
-}
 
 /**
  * Display the list of Storyline categories.
@@ -126,75 +107,5 @@ function comicpress_list_storyline_categories($args = "") {
   if ($style == "list") { $output .= "</ul></li>"; }
   echo $output;
 }
-
-/**
-* Display the comic transcript
-* Transcript must be entered into a custom field named "transcript"
-* @param string $displaymode, "raw" (straight from the field), "br" (includes html line breaks), "styled" (fully css styled with JavaScript expander)
-*/
-function the_transcript($displaymode = 'raw') {
-	$transcript = get_post_meta( get_the_ID(), "transcript", true );
-  switch ($displaymode) {
-    case "raw":
-      echo $transcript;
-      break;
-    case "br":
-      echo nl2br($transcript);
-      break;
-    case "styled":
-      if (!empty($transcript)) { ?>
-        <script type='text/javascript'>
-          <!--
-            function toggle_expander(id) {
-              var e = document.getElementById(id);
-              if(e.style.height == 'auto')
-                e.style.height = '1px';
-              else
-              e.style.height = 'auto';
-            }
-          //-->
-        </script>
-        <div class="transcript-border"><div id="transcript"><a href="javascript:toggle_expander('transcript-content');" class="transcript-title">&darr; Transcript</a><div id="transcript-content"><?php echo nl2br($transcript); ?><br /><br /></div></div></div>
-        <script type='text/javascript'>
-          <!--
-            document.getElementById('transcript-content').style.height = '1px';
-          //-->
-        </script><?php
-      }
-      break;
-  }
-}
-	
-// Register Sidebar and Define Widgets
-	
- function widget_comicpress_latest_comics() { ?>
-	<li>
-		<h2>Latest Comics</h2>
-		<ul>	
-			 <?php global $post;
-			$latestcomics = get_posts('numberposts=5&category='.get_all_comic_categories_as_cat_string());
-			foreach($latestcomics as $post) : ?>
-				<li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
-			<?php endforeach; ?>
-     	</ul>
-	</li>
-	<?php } if ( function_exists('register_sidebar_widget') )
-	register_sidebar_widget(__('Latest Comics'), 'widget_comicpress_latest_comics');
- 
-function widget_comicpress_random_comic() { ?> 
-	<li>
-		<h2><a href="?randomcomic"><span class="random-comic-icon">?</span> Random Comic</a></h2>
-	</li>
-	<?php } if ( function_exists('register_sidebar_widget') )
-	register_sidebar_widget(__('Random Comic'), 'widget_comicpress_random_comic');
-  
-function widget_comicpress_archive_dropdown() { ?>
-	<li class="archive-dropdown-wrap">
-		<select name="archive-dropdown" class="archive-dropdown" onChange='document.location.href=this.options[this.selectedIndex].value;'> 
-		<option value=""><?php echo attribute_escape(__('Archives...')); ?></option> 
-		<?php wp_get_archives('type=monthly&format=option&show_post_count=1'); ?> </select>
-	</li>
-	<?php } if ( function_exists('register_sidebar_widget') )
-	register_sidebar_widget(__('Archive Dropdown'), 'widget_comicpress_archive_dropdown');
 
 ?>
