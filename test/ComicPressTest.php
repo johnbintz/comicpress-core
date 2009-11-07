bd1c206ba4ac91547d71c54608f1795ddf8eec90
John Bintz <john@dawn.local>
2009-11-07 17:00:39 -0500

    remove cascade search, supplanted by find file

--- test/ComicPressTest.php
+++ test/ComicPressTest.php
@@ -14,37 +14,6 @@ class ComicPressTest extends PHPUnit_Framework_TestCase {
     vfsStreamWrapper::setRoot(new vfsStreamDirectory('root'));
   }
 
-  function providerTestCascadeSearch() {
-  	$parent = vfsStream::url('root/parent/file');
-  	$child = vfsStream::url('root/child/file');
-  	
-  	return array(
-  		array(array(), false, false),
-  		array(array('child'), false, $child),
-  		array(array('parent'), false, $parent),
-  		array(array('child', 'parent'), false, $child),
-  		array(array('child', 'parent'), true, $parent),
-  	);
-  }
-
-  /**
-   * @dataProvider providerTestCascadeSearch
-   */
-  function testCascadeSearch($create, $force_parent, $expected_result) {
-  	mkdir(vfsStream::url('root/parent'), 0777);
-  	mkdir(vfsStream::url('root/child'), 0777);
-  	
-  	_set_template_directory(vfsStream::url('root/parent'));
-  	_set_stylesheet_directory(vfsStream::url('root/child'));
-  	
-  	foreach ($create as $type) {
-  	  file_put_contents(vfsStream::url("root/${type}/file"), 'file');
-  	}
-  	
-  	$result = $this->cp->cascade_search('file', $force_parent);
-  	$this->assertTrue($result === $expected_result);
-  }
-  
   function providerTestCategorySearch() {
   	return array(
   	  array(
