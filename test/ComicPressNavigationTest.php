364c1479a2651367d9e64d72c39a0477524d8a15
John Bintz <john@coswellproductions.com>
2009-11-21 09:56:31 -0500

    fix the fact that you con't pass restrictions to navigation

--- test/ComicPressNavigationTest.php
+++ test/ComicPressNavigationTest.php
@@ -35,25 +35,27 @@ class ComicPressNavigationTest extends PHPUnit_Framework_TestCase {
     $dbi = $this->getMock('ComicPressDBInterface', array('get_previous_comic', 'get_next_comic', 'get_first_comic', 'get_last_comic'));
     $storyline = new ComicPressStoryline();
 
-    $storyline->_structure = array(
-      '1' => array('next' => 2),
-      '2' => array('previous' => 1, 'next' => 3),
-      '3' => array('previous' => 2)
-    );
+    $storyline->set_flattened_storyline('0/1,0/1/2,0/3');
 
     wp_insert_post(array('ID' => 1));
     $post = get_post(1);
 
     wp_set_post_categories(1, array(2));
 
-    $dbi->expects($this->at(0))->method('get_previous_comic')->with(null, $post);
-    $dbi->expects($this->at(1))->method('get_next_comic')->with(null, $post);
-    $dbi->expects($this->at(2))->method('get_first_comic')->with(null);
-    $dbi->expects($this->at(3))->method('get_last_comic')->with(null);
+    $dbi->expects($this->at(0))->method('get_previous_comic')->with(array(1,2,3), $post);
+    $dbi->expects($this->at(1))->method('get_next_comic')->with(array(1,2,3), $post);
+    $dbi->expects($this->at(2))->method('get_first_comic')->with(array(1,2,3));
+    $dbi->expects($this->at(3))->method('get_last_comic')->with(array(1,2,3));
     $dbi->expects($this->at(4))->method('get_previous_comic')->with(2, $post);
     $dbi->expects($this->at(5))->method('get_next_comic')->with(2, $post);
-    $dbi->expects($this->at(6))->method('get_first_comic')->with(1)->will($this->returnValue((object)array('ID' => 1)));
-    $dbi->expects($this->at(7))->method('get_first_comic')->with(3)->will($this->returnValue((object)array('ID' => 1)));
+    // level
+    $dbi->expects($this->at(6))->method('get_first_comic')->with(2)->will($this->returnValue((object)array('ID' => 1)));
+    // parent
+    $dbi->expects($this->at(7))->method('get_first_comic')->with(1)->will($this->returnValue((object)array('ID' => 1)));
+    // previous
+    $dbi->expects($this->at(8))->method('get_first_comic')->with(1)->will($this->returnValue((object)array('ID' => 1)));
+    // next
+    $dbi->expects($this->at(9))->method('get_first_comic')->with(3)->will($this->returnValue((object)array('ID' => 1)));
 
     $this->nav->_dbi = $dbi;
     $this->nav->_storyline = $storyline;
