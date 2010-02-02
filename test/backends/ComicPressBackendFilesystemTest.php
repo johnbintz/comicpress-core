1e3f1f0ab04552dcebd1c3cfeb896e3653be33d5
John Bintz <john@coswellproductions.com>
2010-02-01 22:41:56 -0500

    code coverage

--- test/backends/ComicPressBackendFilesystemTest.php
+++ test/backends/ComicPressBackendFilesystemTest.php
@@ -15,5 +15,40 @@ class ComicPressBackendFilesystemTest extends PHPUnit_Framework_TestCase {
 		vfsStreamWrapper::setRoot(new vfsStreamDirectory('root'));
 	}
 
-	function testSomething() {}
+	function providerTestDims() {
+		return array(
+			array(false, array()),
+			array(
+				array(
+					200, 100, 300
+				),
+				array(
+					'width' => 200,
+					'height' => 100
+				)
+			)
+		);
+	}
+
+	/**
+	 * @dataProvider providerTestDims
+	 */
+	function testDims($getimagesize_return, $expected_result) {
+		$fs = $this->getMock('ComicPressBackendFilesystem', array('_getimagesize', 'ensure_type'));
+		$fs->expects($this->once())->method('ensure_type')->with('type')->will($this->returnValue('newtype'));
+		$fs->expects($this->once())->method('_getimagesize')->with('file')->will($this->returnValue($getimagesize_return));
+
+		$fs->files_by_type = array('newtype' => 'file');
+
+		$this->assertEquals($expected_result, $fs->dims('type'));
+	}
+
+	function testUrl() {
+		$fs = $this->getMock('ComicPressBackendFilesystem', array('ensure_type'));
+		$fs->expects($this->once())->method('ensure_type')->with('type')->will($this->returnValue('newtype'));
+
+		$fs->file_urls_by_type = array('newtype' => 'url');
+
+		$this->assertEquals('url', $fs->url('type'));
+	}
 }
